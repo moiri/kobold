ecf3635419e988275a1060827e966815bbd26332
Simon Maurer <moirelein@gmail.com>
2013-08-22 21:14:07 +0200

    add test elevators

--- js/test.js
+++ js/test.js
@@ -5,6 +5,37 @@ $(document).ready(function() {
             $('#content')
             .append('<div id="stone' + j + '" class="stone' + i + ' solid"></div>');
     $('#content').append('<div id="elevator1" class="elevator1 solid moving"></div>');
+    moveUp = function () {
+        $('#elevator1').animate(
+            {"bottom" : "+=500px"},
+            "slow",
+            moveDown
+        );
+    };
+    moveDown = function () {
+        $('#elevator1').animate(
+            {"bottom" : "-=500px"},
+            "slow",
+            moveUp
+        );
+    };
+    moveUp();
+    $('#content').append('<div id="elevator2" class="elevator2 solid moving"></div>');
+    moveRight = function () {
+        $('#elevator2').animate(
+            {"left" : "-=500px"},
+            "slow",
+            moveLeft
+        );
+    };
+    moveLeft = function () {
+        $('#elevator2').animate(
+            {"left" : "+=500px"},
+            "slow",
+            moveRight
+        );
+    };
+    moveRight();
     $('#content').append('<div id="pickUp1" class="pickUp1 pickUp"></div>');
     $('#content').append('<div id="pickUp2" class="pickUp2 pickUp"></div>');
     $('#solidCnt').text((i-1)*j);
